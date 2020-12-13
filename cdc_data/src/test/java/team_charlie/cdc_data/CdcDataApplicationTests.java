446c60f14f1bf941c6bfff8412133d6a5babfc68
Michael Parish <wasadmin@webagesolutions.com>
2020-12-13 06:17:10 -0800

    first commit

---
+++ cdc_data/src/test/java/team_charlie/cdc_data/CdcDataApplicationTests.java
@@ -0,0 +1,13 @@
+package team_charlie.cdc_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
