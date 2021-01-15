08a5cd7447f7349816c1bf32250bc21a078e1164
610068 <Parish_Michael@bah.com>
2021-01-14 21:17:03 -0500

    changing container name

--- src/main/java/team_charlie/cdc_data/CdcDataApplication.java
+++ src/main/java/team_charlie/cdc_data/CdcDataApplication.java
@@ -17,8 +17,8 @@ public class CdcDataApplication {
 	}
 
 	@GetMapping("/hello")
-	public String sayHello(@RequestParam(value = "myName", defaultValue = "there!") String name) {
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "there Steve!") String name) {
 		this.name = name;
-		return String.format("Hello %s!", name);
+		return String.format("Hello %s Mike!", name);
 	}
 }
