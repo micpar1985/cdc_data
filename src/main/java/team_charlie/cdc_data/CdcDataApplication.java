3348eff1b1077ee5c78e85a940093219a6b2a4e4
610068 <Parish_Michael@bah.com>
2021-01-17 15:29:13 -0500

    adding new defaultcredentials call

--- src/main/java/team_charlie/cdc_data/CdcDataApplication.java
+++ src/main/java/team_charlie/cdc_data/CdcDataApplication.java
@@ -4,13 +4,17 @@ package team_charlie.cdc_data;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Base64;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
 import org.springframework.web.client.RestTemplate;
 
 import org.springframework.boot.SpringApplication;
@@ -25,11 +29,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CdcDataApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CdcDataApplication.class, args);
+
+		getData();
+
 	}
 
-	/*@GetMapping("/getcdcdata")
-	public String getData() {
+	//@GetMapping("/getcdcdata")
+	public static String getData() {
 
 		final String uri = "https://data.cdc.gov/resource/vbim-akqf.json";
 		RestTemplate restTemplate = new RestTemplate();
@@ -47,23 +55,29 @@ public class CdcDataApplication {
 			e.printStackTrace();
 		}
 
-		AWSCredentials credentials = new BasicAWSCredentials(
+		/*AWSCredentials credentials = new BasicAWSCredentials(
 				getAwsKeyId(),
 				getAwsKeySecret()
-		);
+		);*/
+
+		AWSCredentialsProvider credentialsProvider = InstanceProfileCredentialsProvider.getInstance();
 
 		AmazonS3 s3client = AmazonS3ClientBuilder
 				.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(credentials))
-				.withRegion(getAwsS3Region())
+				.withCredentials(new DefaultAWSCredentialsProviderChain())
+				.withRegion("us-west-2")
 				.build();
 
-		s3client.putObject(
+		s3client.toString();
+
+		String acctOwner = s3client.getS3AccountOwner().getDisplayName();
+
+		/*s3client.putObject(
 				AmazonS3Config.getAwsS3Bucket(),
 				AmazonS3Config.getAwsS3Object(),
 				new File(filepath)
-		);
+		);*/
 
 		return ("CDC data is now refreshed!");
-	}*/
+	}
 }
