cc8343da6efc1dd15682b1f65baf0f4d8e8145e9
610068 <Parish_Michael@bah.com>
2021-01-18 09:14:31 -0500

    getting cdc data and adding to bucket

--- src/main/java/team_charlie/cdc_data/CdcDataApplication.java
+++ src/main/java/team_charlie/cdc_data/CdcDataApplication.java
@@ -1,80 +1,58 @@
 package team_charlie.cdc_data;
 
 //import team_charlie.cdc_data.model.*;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.Base64;
 
-import com.amazonaws.auth.*;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.secretsmanager.AWSSecretsManager;
-import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.amazonaws.services.secretsmanager.model.*;
-import org.springframework.web.client.RestTemplate;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//import static team_charlie.cdc_data.model.AmazonS3Config.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 @RestController
 public class CdcDataApplication {
 
-	public static void main(String[] args) {
+    private static String filepath = "No path assigned";
 
-		SpringApplication.run(CdcDataApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(CdcDataApplication.class, args);
+        System.out.println(getData());
+    }
 
-		getData();
+    //@GetMapping("/getcdcdata")
+    public static String getData() {
 
-	}
+        final String uri = "https://data.cdc.gov/resource/vbim-akqf.json";
+        RestTemplate restTemplate = new RestTemplate();
 
-	//@GetMapping("/getcdcdata")
-	public static String getData() {
+        try {
+            String urlresult = restTemplate.getForObject(uri, String.class);
 
-		final String uri = "https://data.cdc.gov/resource/vbim-akqf.json";
-		RestTemplate restTemplate = new RestTemplate();
-		String urlresult = restTemplate.getForObject(uri, String.class);
-		String filepath = "No path assigned";
+            File tmpFile = File.createTempFile("cdcdata", ".json");
+            FileWriter writer = new FileWriter(tmpFile);
+            writer.write(urlresult);
+            writer.close();
 
-		try {
-			File tmpFile = File.createTempFile("cdcdata", ".json");
-			FileWriter writer = new FileWriter(tmpFile);
-			writer.write(urlresult);
-			writer.close();
+            filepath = tmpFile.getAbsolutePath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-			filepath = tmpFile.getAbsolutePath();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withRegion("us-west-2")
+                .build();
 
-		/*AWSCredentials credentials = new BasicAWSCredentials(
-				getAwsKeyId(),
-				getAwsKeySecret()
-		);*/
+        s3client.putObject("my-cdc-bucket",
+                "cdc-data-file",
+                new File(filepath));
 
-		AWSCredentialsProvider credentialsProvider = InstanceProfileCredentialsProvider.getInstance();
-
-		AmazonS3 s3client = AmazonS3ClientBuilder
-				.standard()
-				.withRegion("us-west-2")
-				.build();
-
-		s3client.toString();
-
-		String acctOwner = s3client.getS3AccountOwner().getDisplayName();
-
-		s3client.putObject("my-cdc-bucket",
-				"mlkjr.txt",
-				"/Document/mlkjr.txt");
-
-		return ("CDC data is now refreshed!");
-	}
+        return ("CDC data is now refreshed!");
+    }
 }
