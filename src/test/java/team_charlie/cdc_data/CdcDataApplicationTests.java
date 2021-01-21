94f080b84436d23db1cb26b3a58e03a35b72e021
610068 <Parish_Michael@bah.com>
2021-01-21 14:42:57 -0500

    adding unit test

--- src/test/java/team_charlie/cdc_data/CdcDataApplicationTests.java
+++ src/test/java/team_charlie/cdc_data/CdcDataApplicationTests.java
@@ -1,13 +1,45 @@
 package team_charlie.cdc_data;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/*@SpringBootTest
+
+@SpringBootTest
 class CdcDataApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}*/
+	@Test
+	public void testGetData() throws Exception {
+		System.out.println("This is a test!");
+
+		// Given
+		final String jsonMimeType = "application/json";
+		HttpUriRequest request = new HttpGet( "https://data.cdc.gov/resource/vbim-akqf.json" );
+
+		// When
+		HttpResponse response = HttpClientBuilder.create().build().execute( request );
+
+		// Then
+		String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+
+		assertEquals(jsonMimeType, mimeType);
+
+	}
+
+	private void assertEquals(String jsonMimeType, String mimeType) {
+	}
+
+
+}
+
+
+
+
